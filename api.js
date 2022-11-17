b8d7c102e4cb4aadf49d47c84c5c3f30f16edd96
architsinghhh <singharchit16163@gmail.com>
2022-11-17 21:46:38 +0530

    Add your commit

---
+++ api.js
@@ -0,0 +1,48 @@
+const client = require('./connection');
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.listen(3000, () => {
+
+    console.log('Server is running on port 3000');
+})
+
+
+
+
+client.connect();
+
+app.get('/athlete', (req, res) => {
+
+
+    client.query(`set search_path to olympics_db`,(err,result)=>{
+
+        if(!err)
+        {
+            console.log("Connected to database");
+        }
+        else{
+            console.log(err);
+        }
+        
+        
+    });
+
+    client.query(`Select * from "athlete"`, (err, result) => {
+
+        if (!err) {
+            res.send(result.rows);
+        }
+        else {
+            console.log(err);
+        }
+    })
+
+    client.end;
+})
+
+
---
+++ connection.js
@@ -0,0 +1,17 @@
+const {Client} = require('pg');
+
+
+const client = new Client({
+    
+
+    host:'localhost',
+    user:'postgres',
+    port:5432,
+    password:'admin',
+    database:'olympics_db',
+    
+    
+});
+
+
+module.exports = client
